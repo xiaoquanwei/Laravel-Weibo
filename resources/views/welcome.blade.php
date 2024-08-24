17a0d7db99f4d6b9e594402b57cbd0a3b075177f
xqw <xqw@qq.com>
2024-08-24 22:18:43 +0800

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div>Hello World</div>
+    </div>
+</div>
+</body>
+</html>
