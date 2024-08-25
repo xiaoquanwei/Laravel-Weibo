f5554ee64941b95ad0a4717bc46f0b21d8d862ba
xqw <xqw@qq.com>
2024-08-25 18:10:32 +0800

    路由给名称->name('xx'), 在视图文件中就可以用这个名称来生成链接了：{{ route('xx') }}

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', 'StaticPagesController@home');  // get方式，第一个参数指明url，StaticPagesController是控制器，home是方法
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');  // get方式，第一个参数指明url，StaticPagesController是控制器，home是方法
+Route::get('/help', 'StaticPagesController@help')->name('help');  // 在路由后面链式调用 name 方法来为路由指定名称
+Route::get('/about', 'StaticPagesController@about')->name('about');
