9e414431dd0bb7d32437899725b873d19acbbb26
xqw <xqw@qq.com>
2024-08-26 20:31:05 +0800

    User模型文件中的fillable，hidden，casts熟悉解释

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,19 +1,22 @@
 <?php
+/**
+ * User 数据模型文件
+ */
 
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;  // 模型工厂相关功能的引用
+use Illuminate\Foundation\Auth\User as Authenticatable; // 授权相关功能的引用
+use Illuminate\Notifications\Notifiable;  // 消息通知相关功能引用
+use Laravel\Sanctum\HasApiTokens;  //  API 令牌修改功能
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * 字段白名单，只有在这个里面的字段才能被修改
      *
      * @var array<int, string>
      */
@@ -24,7 +27,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * 这里的字段，会在返回时隐藏
      *
      * @var array<int, string>
      */
@@ -34,11 +37,11 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * 指定数据库字段使用的数据类型
      *
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'email_verified_at' => 'datetime',  // email_verified_at 使用的是 datetime 时间类型
     ];
 }
