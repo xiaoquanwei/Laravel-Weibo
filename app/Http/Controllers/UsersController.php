56788e8062eb5970192b8cc9e6f439268c800273
xqw <xqw@qq.com>
2024-08-29 18:50:24 +0800

    用户登录视图优化，用户注册成功自动登录

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -34,6 +35,9 @@ class UsersController extends Controller
             'email'=> $request->email,
             'password'=>bcrypt($request->password),
         ]);
+
+        // 注册成功后自动登录
+        Auth::login($user);
 
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 
