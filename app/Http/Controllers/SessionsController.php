a0a6ef790a4b8f0d5505a5f24369bad3d0ecede0
xqw <xqw@qq.com>
2024-09-11 10:35:54 +0800

    添加记住我功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,7 +21,7 @@ class SessionsController extends Controller
         ]);
 
         // 用户身份认证
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
