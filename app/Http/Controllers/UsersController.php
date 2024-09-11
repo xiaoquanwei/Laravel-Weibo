3ed0844424ffd09f5552598622a132969607bcfb
xqw <xqw@qq.com>
2024-09-11 12:30:26 +0800

    列出所有用户，批量生成测试用户。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,7 +14,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
@@ -85,6 +85,10 @@ class UsersController extends Controller
         return redirect()->route('users.show', $user);
     }
 
+    public function index() {
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
 
 
 }
